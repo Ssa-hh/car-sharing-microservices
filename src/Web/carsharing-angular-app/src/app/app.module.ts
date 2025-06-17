3bd5e75c8f89c6752082189edcfca0424e93b57a
Ssa-hh <samir-salmi@hotmail.com>
2025-06-17 15:47:43 +0200

    Web app - add header and footer

--- src/Web/carsharing-angular-app/src/app/app.module.ts
+++ src/Web/carsharing-angular-app/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { search, personCircle } from 'ngx-bootstrap-icons';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
+
+const icons = {
+  search,
+  personCircle
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbNavModule,
+    NgbCollapseModule,
+    NgxBootstrapIconsModule.pick(icons)
   ],
   providers: [],
   bootstrap: [AppComponent]
